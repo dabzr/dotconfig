a022bec9567c23746addd50167df39335922f1ff
dabzr <dylanab@pm.me>
2026-05-29 15:40:27 -0300

    adicionando configurações do lazyvim e do fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
+end
+function gk
+    set key_path ~/Projects/.key
+
+    if not test -f $key_path
+        echo "❌ Arquivo não encontrado: $key_path"
+        return 1
+    end
+
+    if sudo cat $key_path | wl-copy
+        echo "✅ Chave copiada para a área de transferência!"
+    else
+        echo "❌ Erro ao copiar a chave."
+    end
 end
